04540b3dccfdf7225a87165c478b2fec2918a433
Marcos <>
2023-11-26 18:26:01 -0500

    Mejora vista perfil

--- app/src/main/java/com/unal/jardiapp/NewPlantActivity.java
+++ app/src/main/java/com/unal/jardiapp/NewPlantActivity.java
@@ -2,6 +2,7 @@ package com.unal.jardiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -9,20 +10,31 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.unal.jardiapp.model.Plant;
+
 public class NewPlantActivity extends AppCompatActivity {
 
-    private String[] item_recordatorio = {"Cada día", "Cada 3 días", "Cada 5 días", "Cada semana"};
-    private AutoCompleteTextView autoCompleteTextViewRecordatorio;
-    private ArrayAdapter<String> adarpterRecordatorio;
+    EditText plantNameTxt, speciesTxt;
+    Button addPlantButtn, addImageBttn;
+    ImageView image;
+    private final int GALLERY_REQ_CODE = 1;
+    private AutoCompleteTextView autoCompleteTextViewRecordatorio, autoCompleteTextViewPrioridad;
+    private ArrayAdapter<String> adarpterRecordatorio, adarpterPrioridad;
 
+    private String[] item_recordatorio = {"Cada día", "Cada 3 días", "Cada 5 días", "Cada semana"};
     private String[] item_prioridad = {"Baja", "Media", "Alta"};
-    private AutoCompleteTextView autoCompleteTextViewPrioridad;
-    private ArrayAdapter<String> adarpterPrioridad;
 
     private String nameText;
     private String emailText;
@@ -37,9 +49,21 @@ public class NewPlantActivity extends AppCompatActivity {
         nameText = getIntent().getExtras().getString("nombres");
         emailText = getIntent().getExtras().getString("email");
         //photoImageView = (ImageView) mToolbar.findViewById(R.id.profile_circlex);
+
+        plantNameTxt = findViewById(R.id.nombreEditTxt);
+        speciesTxt = findViewById(R.id.especieEditTxt);
+        addPlantButtn = findViewById(R.id.createPlantButton);
+        image = findViewById(R.id.plantImg);
+        addImageBttn = findViewById(R.id.addImgBttn);
+
         autoCompleteTextViewRecordatorio = findViewById(R.id.auto_complete_recordatorio);
         adarpterRecordatorio = new ArrayAdapter<>(this, R.layout.list_item, item_recordatorio);
         autoCompleteTextViewRecordatorio.setAdapter(adarpterRecordatorio);
+
+        autoCompleteTextViewPrioridad = findViewById(R.id.auto_complete_prioridad);
+        adarpterPrioridad = new ArrayAdapter<>(this, R.layout.list_item, item_prioridad);
+        autoCompleteTextViewPrioridad.setAdapter(adarpterPrioridad);
+
         autoCompleteTextViewRecordatorio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -47,9 +71,6 @@ public class NewPlantActivity extends AppCompatActivity {
             }
         });
 
-        autoCompleteTextViewPrioridad = findViewById(R.id.auto_complete_prioridad);
-        adarpterPrioridad = new ArrayAdapter<>(this, R.layout.list_item, item_prioridad);
-        autoCompleteTextViewPrioridad.setAdapter(adarpterPrioridad);
         autoCompleteTextViewPrioridad.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -57,6 +78,37 @@ public class NewPlantActivity extends AppCompatActivity {
             }
         });
 
+        addImageBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(NewPlantActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
+        addPlantButtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (savePlant())
+                    Toast.makeText(NewPlantActivity.this, "Planta creada y almacenada", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(NewPlantActivity.this, "Se presentó un error, no se pudo crear el registro de la planta", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            //if (requestCode == GALLERY_REQ_CODE){
+            image.setImageURI(data.getData());
+            //}
+        }
     }
 
     @Override
@@ -106,4 +158,20 @@ public class NewPlantActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Method for save plant in Database
+     * @return true if an autocremental id was created and the registry was saved,
+     * otherwise, return false.
+     */
+    private boolean savePlant(){
+        Plant plant = new Plant();
+        plant.setName(plantNameTxt.getText().toString());
+        plant.setSpecies(speciesTxt.getText().toString());
+        plant.setPriority(autoCompleteTextViewPrioridad.getText().toString());
+        plant.setReminder(autoCompleteTextViewRecordatorio.getText().toString());
+        plant.setSensor(""); // TODO: expecting to manage the sensor information
+        // TODO: call the DAO for insert and retrieve the autoincrementable id
+        return false; // FIXME: if id != null then true, else false.
+
+    }
 }
---
+++ app/src/main/java/com/unal/jardiapp/model/Plant.java
@@ -0,0 +1,68 @@
+package com.unal.jardiapp.model;
+
+public class Plant {
+
+    private int id;
+    private String name;
+    private String species;
+    private String priority;
+    private String reminder;
+    private String sensor;
+    private String imgScr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(String reminder) {
+        this.reminder = reminder;
+    }
+
+    public String getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(String sensor) {
+        this.sensor = sensor;
+    }
+
+    public String getImgScr() {
+        return imgScr;
+    }
+
+    public void setImgScr(String imgScr) {
+        this.imgScr = imgScr;
+    }
+}
